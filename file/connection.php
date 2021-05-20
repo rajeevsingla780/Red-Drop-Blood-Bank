670564d3852c8936ac93a370260dbf088187da8a
Rajeev Singla <63499957+rajeevsingla780@users.noreply.github.com>
2021-05-20 17:40:54 +0530

    Update connection.php

--- file/connection.php
+++ file/connection.php
@@ -1,6 +1,19 @@
-<?php
+<!-- <?php
 
 $conn=mysqli_connect('localhost','root','') or die('Cannot connect to server');
 mysqli_select_db($conn,'bloodbank') or die ('Cannot found database');
 
+?> -->
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
